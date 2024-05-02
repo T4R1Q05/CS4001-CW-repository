e9d762669368cffa8192cc0d4045dbbfd5cf8cf9
T4R1Q <77877840+T4R1Q05@users.noreply.github.com>
2024-05-02 17:58:28 +0100

    Gadget.java

--- Gadget.java
+++ Gadget.java
@@ -1,3 +1,8 @@
+/*
+ * Author: Tariq Chowdhury
+ * Date: 2/05/2024
+ * Purpose: This class is a superclass and represents a gadget. It has attributes model, price, weight and size. The class has a constructor, getPrice method, getWeight method, getSize method and display method.
+ */
 public class Gadget {
     private String model;
     private double price;
