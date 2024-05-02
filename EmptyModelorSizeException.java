a4f998d2bf0e53aa90f60e4b628414333d12a341
T4R1Q <77877840+T4R1Q05@users.noreply.github.com>
2024-05-02 17:59:18 +0100

    EmptyModelorSizeException.java

--- EmptyModelorSizeException.java
+++ EmptyModelorSizeException.java
@@ -1,3 +1,8 @@
+/*
+ * Author: Tariq Chowdhury
+ * Date: 2/05/2024
+ * Purpose: This class is a superclass and represents a gadget. It has attributes model, price, weight and size. The class has a constructor, getPrice method, getWeight method, getSize method and display method.
+ */
 public class EmptyModelorSizeException extends Exception {
     public EmptyModelorSizeException() {
         super("Please enter a model and size.");
