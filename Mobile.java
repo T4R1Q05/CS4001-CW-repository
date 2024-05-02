dd7a6ee302438965da7e40b9448017383b57ae6c
T4R1Q <77877840+T4R1Q05@users.noreply.github.com>
2024-05-02 17:57:42 +0100

    Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -1,3 +1,8 @@
+/*
+ * Author: Tariq Chowdhury
+ * Date: 2/05/2024
+ * Purpose: This class is a subclass of Gadget and represents a mobile phone. It has an additional attribute creditRemaining which represents the credit available on the mobile phone. The class has a constructor, getCreditRemaining method, addCredit method, makeCall method and display method.
+ */
 public class Mobile extends Gadget {
     private int creditRemaining;
 
